7510ebfcbce878a13f740bb558261ddbe16eff4c
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-11 04:38:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,5 +11,6 @@ end
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css(".location-name").text 
 
 create_project_hash 
