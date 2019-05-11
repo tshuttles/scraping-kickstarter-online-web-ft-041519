004cdfdd4eff6b6b9267f7813c0924416370e312
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-11 04:41:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {} 
+  
+  end
+  projects 
 end
 
 # projects: kickstarter.css("li.project.grid_4")
