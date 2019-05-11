cdebabe83f3f29afc7fa49ebcce14e0401850461
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-11 04:45:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,11 +2,13 @@ require 'nokogiri'
 require 'pry'
 
 
-  # html = File.read('fixtures/kickstarter.html')
-  # kickstarter = Nokogiri::HTML(html)
   
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  
   projects = {} 
+  
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {}
