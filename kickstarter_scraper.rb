c3b615a8170b74cf4aaa32d7263aa2ec132a1bcd
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-11 04:40:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry 
+  projects = {} 
 end
 
 # projects: kickstarter.css("li.project.grid_4")
@@ -12,5 +12,6 @@ end
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css(".location-name").text 
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 create_project_hash 
